2abf6705d478887d6e434d7ffa44056b5d900585
ZynImran <imransamia787658@gmail.com>
2025-12-14 08:05:04 +0600

    Add files via upload

---
+++ 3.(a)factorial of a number  Using iteration (without recursion).cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+     int n;
+    long long fact = 1;
+    cout << "Enter a number: ";
+    cin >> n;
+    if (n < 0) {
+        cout << "Factorial is not defined for negative numbers.";
+    } else {
+        for (int i = 1; i <= n; i++) {
+            fact *= i;
+        }
+        cout << "Factorial of " << n << " = " << fact;
+    }
+}
